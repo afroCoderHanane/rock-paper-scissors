cc77796c77ab4bf200dde58c3f88a069088cc77b
Abdoul Hanane <gbadhanane@gmail.com>
2021-01-04 02:35:29 +0000

    added security for input

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,4 +26,26 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+game_ai=[rock,paper,scissors]
+user=[rock,paper,scissors]
+# game_list = [[game_ai],[user]]
 
+# ai_choice = random.randint(0,len(game_ai)-1)
+ai_choice = random.choice(game_ai);
+
+user_choice = int(input("Enter your choice\n 1: for rock 2: for paper 3: for scissors "))
+if user_choice <0 or user_choice>3:
+  print("You entered an invalid input ")
+else :
+  print("AI choose\n"+ ai_choice)
+  print("You choose\n"+ user[user_choice-1])
+  if(ai_choice==game_ai[0] and user[user_choice-1]==user[1] ):
+    print(" You won")
+  elif (ai_choice==game_ai[1] and user[user_choice-1]==user[2] ):
+    print(" You won")
+  elif (ai_choice==game_ai[2] and user[user_choice-1]==user[0] ):
+    print("You won")
+  elif(ai_choice == user[user_choice -1]):
+    print("It is a draw")
+  else:
+    print("AI machine won, You lost")
